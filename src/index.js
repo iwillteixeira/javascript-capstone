83819f9abef775b9e048a79dbea8bceff96b4794
(Will)Juscelino Junior <89435894+iwillteixeira@users.noreply.github.com>
2021-12-21 10:35:12 -0300

    Revert "Feature: Comment"

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
 import './scss/style.scss';
-import Comment from './comment.js';
-
-const btnComment = document.querySelectorAll('#comments');
-
-btnComment.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    Comment.showModal(e);
-  });
-});
