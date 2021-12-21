586c1bf8a5404156a9be30d7efce979583940562
George Magdy <georgtriple@gmail.com>
2021-12-21 21:07:16 +0200

    Fix APIHelper file

--- src/js/APIHelper.js
+++ src/js/APIHelper.js
@@ -1,22 +1,22 @@
 export default class APIHelper {
-  static baseURL = 'https://api.tvmaze.com/shows';
+  static baseURL = "https://api.tvmaze.com/shows";
 
-  static async getAll(page = 1) {
+  static getAll(page = 1) {
     try {
-      this.data = await fetch(`${APIHelper.baseURL}?page=${page}`);
-      const allData = this.data.json();
-      return allData;
+      return fetch(`${APIHelper.baseURL}?page=${page}`).then((response) =>
+        response.json()
+      );
     } catch {
       console.log("Error: Couldn't fetch films data");
     }
     return [];
   }
 
-  static async getDetails(showId) {
+  static getDetails(showId) {
     try {
-      this.data = await fetch(`${APIHelper.baseURL}/${showId}`);
-      const allData = this.data.json();
-      return allData;
+      return fetch(`${APIHelper.baseURL}/${showId}`).then((response) =>
+        response.json()
+      );
     } catch {
       console.log("Error: Couldn't fetch film details");
     }
