20b59a7c01de225be54e36df00d3239aa1d3d70e
George Magdy <georgtriple@gmail.com>
2021-12-21 21:06:52 +0200

    Add Involvement API

---
+++ src/js/InvolvementAPIHelper.js
@@ -0,0 +1,35 @@
+export default class InvolvementAPIHelper {
+  static AppId = "x3O8Z7Ily4B1bmO5FugG";
+  static baseURL =
+    "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/";
+
+  static getLikes() {
+    try {
+      return fetch(
+        `${InvolvementAPIHelper.baseURL}apps/${InvolvementAPIHelper.AppId}/likes`
+      ).then((response) => response.json());
+    } catch (ex) {
+      console.log(ex.message);
+    }
+    return [];
+  }
+
+  static postLikes(showId) {
+    try {
+      fetch(
+        `${InvolvementAPIHelper.baseURL}apps/${InvolvementAPIHelper.AppId}/likes`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            item_id: showId,
+          }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        }
+      );
+    } catch (ex) {
+      console.log(ex.message);
+    }
+  }
+}
