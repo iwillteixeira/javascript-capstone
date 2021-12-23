65d4150344df334cef62c30598890a72102dcec1
Yigit Mersin <yigitmersin@gmail.com>
2021-12-23 15:03:25 +0300

    Change test wording

--- src/__tests__/comment.test.js
+++ src/__tests__/comment.test.js
@@ -6,7 +6,7 @@ import countComment from '../__mocks__/comment.js';
 
 jest.mock('../js/comment.js');
 
-describe('counts the all comments', () => {
+describe('it counts all comments for a single card', () => {
   document.body.innerHTML = `<div id="all-comments">
     <p class="h3 mb-5"></p>
     <ul id="comment-list">
@@ -17,7 +17,7 @@ describe('counts the all comments', () => {
     </ul>
   </div>`;
 
-  it('counts li elements inside the mock document', () => {
+  it('counts li elements inside the comment list', () => {
     expect(countComment()).toBe('Comments(4)');
   });
 });
